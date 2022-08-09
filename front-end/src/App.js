15f30ba72d35302d2df712af282135e7b130f3cd
JadenDurnford <jadendurnford@gmail.com>
2022-08-08 23:28:25 -0400

    Hytham initial additions

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+//import EvilPet from 'https://cdn.discordapp.com/attachments/1003507609749442701/1005678437953962034/turntable_2_gif.gif';
+const { Altura } = require("@altura/altura-js");
+
+const altura = new Altura(process.env.ALTURA_API_KEY);
+
+const Log = ({ hapet, streak, onClick, lastLog }) => (
+  <div>
+    <img
+      src="https://cdn.discordapp.com/attachments/1003507609749442701/1005474810811404319/unknown.png"
+      alt="Evil Pet"
+    />
+    <button onClick={onClick}> Log Your Habit </button>
+    <p> Your current streak is {streak} </p>
+    <p> You logged your last Habbit on {lastLog} </p>
+  </div>
+);
 
 function App() {
+  //const [ user, setUser ] = useState()
+  const [lastLog, setLastLog] = useState(" Never ");
+  const [streak, setStreak] = useState(0);
+
+  const handleLog = () => {
+    setStreak(streak + 1);
+    setLastLog(Date());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> HaPets </h1>
+      <Log streak={streak} onClick={handleLog} lastLog={lastLog} />
+      <button onClick={() => console.log(process.env)}></button>
     </div>
   );
 }
