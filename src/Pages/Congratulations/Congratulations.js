f12a01a2d05ccf724afb5ebba5ade772bbdd1650
eddy-guo <eddyyanruguo@gmail.com>
2022-08-09 19:23:19 -0400

    good and bad picture

--- src/Pages/Congratulations/Congratulations.js
+++ src/Pages/Congratulations/Congratulations.js
@@ -9,6 +9,7 @@ export default function Congratulations() {
                 &#127881; Congratulations on completing your HabiPet! &#127881;</h1>
             <div className="imagewrapper">
                 <img src={require('./GoodNft.png')} alt="Good Pet" className="image" />
+                <img src={require('./BadNft.png')} alt="Bad Pet" className="image" />
             </div>
             <Link class="home" to={'/'}><button className="button" role="button">Go home and start/break a new habit!</button></Link>
         </div>
