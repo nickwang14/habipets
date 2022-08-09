6030235b305249e0858df9e55469636df0ccf34c
JadenDurnford <jadendurnford@gmail.com>
2022-08-09 11:22:23 -0400

    adding links to nft boxes to respective pages

--- src/Components/NftBox/NftBox.js
+++ src/Components/NftBox/NftBox.js
@@ -1,25 +1,30 @@
-import { React, Image } from "react";
+import { React } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./NftBox.css";
 
 export default function NftBox(props) {
   const goodHabit = props.type;
   if (goodHabit) {
     return (
-      <div className="GoodNftBox">
-        <img
-          src={require("./GoodNft.png")}
-          alt="Evil Pet"
-          className="NftImage"
-        />
-        <span className="NftBoxText">Embrace a good habit</span>
-      </div>
+      <Link to="/GoodHabit" className="NftBoxLink">
+        <div className="GoodNftBox">
+          <img
+            src={require("./GoodNft.png")}
+            alt="Evil Pet"
+            className="NftImage"
+          />
+          <span className="NftBoxText">Embrace a good habit</span>
+        </div>
+      </Link>
     );
   } else {
     return (
-      <div className="BadNftBox">
-        <img src={require("./BadNft.png")} />
-        <span className="NftBoxText">Fly away from a bad habit</span>
-      </div>
+      <Link to="/BadHabit" className="NftBoxLink">
+        <div className="BadNftBox">
+          <img src={require("./BadNft.png")} />
+          <span className="NftBoxText">Fly away from a bad habit</span>
+        </div>
+      </Link>
     );
   }
 }
---
+++ src/Pages/BadHabit.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function BadHabit() {
+  return (
+    <div>
+      <h1>Bad Habit</h1>
+    </div>
+  );
+}
+
+export default BadHabit;
