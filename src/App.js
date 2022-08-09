8c16ab1366c8a1f6af2ef608bc907a4455153193
nickwang14 <nickwang14@gmail.com>
2022-08-09 12:40:47 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home, Page1, Page2, Page3, HabitPage, NotFound } from './Exports';
+import { Home, HabitPage, NotFound } from './Exports';
 import './App.css';
 //import EvilPet from 'https://cdn.discordapp.com/attachments/1003507609749442701/1005678437953962034/turntable_2_gif.gif';
 // const { Altura } = require("@altura/altura-js");
@@ -11,9 +11,6 @@ function App() {
         <Router>
             <Routes>
                 <Route exact path="/" element={<Home />}></Route>
-                <Route exact path="/p1" element={<Page1 />}></Route>
-                <Route exact path="/p2" element={<Page2 />}></Route>
-                <Route exact path="/p3" element={<Page3 />}></Route>
                 <Route exact path="/HabitPage" element={<HabitPage />}></Route>
                 <Route path="*" element={<NotFound />}></Route>
             </Routes>
