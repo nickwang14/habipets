7cb367727c5d08493fa2d41af3b9c85a3795e6e8
eddy-guo <eddyyanruguo@gmail.com>
2022-08-09 18:12:38 -0400

    final image, general css

--- src/Pages/Congratulations/Congratulations.js
+++ src/Pages/Congratulations/Congratulations.js
@@ -6,6 +6,9 @@ export default function Congratulations() {
     return (
         <div>
             <h1 className="header">Congratulations on completing your HabiPet!</h1>
+            <div className="imagewrapper">
+                <img src={require('./GoodNft.png')} alt="Good Pet" className="image" />
+            </div>
             <Link class="home" to={'/'}>
         Set New Habit
             </Link>
