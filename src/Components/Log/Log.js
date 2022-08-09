fc96ee501bb7b9047a5f0ddaa5399ee3defab620
JadenDurnford <jadendurnford@gmail.com>
2022-08-09 10:32:58 -0400

    adding temp background and nft placeholders

--- src/Components/Log/Log.js
+++ src/Components/Log/Log.js
@@ -1,17 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Log({ hapet, streak, onClick, lastLog }) {
-    return(
-        <div>
-            <img
-                src="https://cdn.discordapp.com/attachments/1003507609749442701/1005474810811404319/unknown.png"
-                alt="Evil Pet"
-            />
-            <button onClick={onClick}> Log Your Habit </button>
-            <p> Your current streak is {streak} </p>
-            <p> You logged your last Habit on {lastLog} </p>
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={onClick}> Log Your Habit </button>
+      <p> Your current streak is {streak} </p>
+      <p> You logged your last Habit on {lastLog} </p>
+    </div>
+  );
 }
 
-export default Log
+export default Log;
