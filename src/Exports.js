cea6844ac43d462d1eec24a647765c016fa4f79b
nickwang14 <nickwang14@gmail.com>
2022-08-09 12:28:25 -0400

    Update and add linters

--- src/Exports.js
+++ src/Exports.js
@@ -1,11 +1,25 @@
-import Header from "./Components/Header/Header";
-import Log from "./Components/Log/Log";
-import Intro from "./Components/Intro/Intro";
-import Home from "./Pages/Home/Home";
-import Page1 from "./Pages/Page1";
-import Page2 from "./Pages/Page2";
-import Page3 from "./Pages/Page3";
-import NotFound from "./Pages/NotFound";
-import NftBox from "./Components/NftBox/NftBox";
+import Header from './Components/Header/Header';
+import Log from './Components/Log/Log';
+import Intro from './Components/Intro/Intro';
+import Home from './Pages/Home/Home';
+import Page1 from './Pages/Page1';
+import Page2 from './Pages/Page2';
+import Page3 from './Pages/Page3';
+import HabitPage from './Pages/HabitPage';
+import NotFound from './Pages/NotFound';
+import NftBox from './Components/NftBox/NftBox';
+import CalendarModal from './Components/Calendar/CalendarModal';
 
-export { Home, Page1, Page2, Page3, NotFound, Header, Log, Intro, NftBox };
+export {
+    Home,
+    Intro,
+    Page1,
+    Page2,
+    Page3,
+    HabitPage,
+    NotFound,
+    Header,
+    Log,
+    NftBox,
+    CalendarModal,
+};
