14ceabbc0100198dfea1dc806b9621d733c3aa05
Leon Dong <leon.d1043@gmail.com>
2022-08-09 09:30:20 -0400

    cheers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
-import Header from './Components/Header/Header';
-import Log from './Components/Log/Log';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Header, Log } from './Exports';
 import "./App.css";
 //import EvilPet from 'https://cdn.discordapp.com/attachments/1003507609749442701/1005678437953962034/turntable_2_gif.gif';
 const { Altura } = require("@altura/altura-js");
@@ -18,11 +23,13 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Header title={"HabiPets"}/>
-      <Log streak={streak} onClick={handleLog} lastLog={lastLog} />
-      <button onClick={() => console.log(process.env)}></button>
-    </div>
+    <Router>
+      <div className="App">
+        <Header title={"HabiPets"}/>
+        <Log streak={streak} onClick={handleLog} lastLog={lastLog} />
+        <button onClick={() => console.log(process.env)}></button>
+      </div>
+    </Router>
   );
 }
 
