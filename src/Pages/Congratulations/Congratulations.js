5702731ea88e18896bb5d7102bf5c275e4ca1658
eddy-guo <eddyyanruguo@gmail.com>
2022-08-09 19:06:19 -0400

    button/header css

--- src/Pages/Congratulations/Congratulations.js
+++ src/Pages/Congratulations/Congratulations.js
@@ -5,13 +5,12 @@ export default function Congratulations() {
     //const { type } = useParams();
     return (
         <div>
-            <h1 className="header">Congratulations on completing your HabiPet!</h1>
+            <h1 className="header">
+                &#127881; Congratulations on completing your HabiPet! &#127881;</h1>
             <div className="imagewrapper">
                 <img src={require('./GoodNft.png')} alt="Good Pet" className="image" />
             </div>
-            <Link class="home" to={'/'}>
-        Set New Habit
-            </Link>
+            <Link class="home" to={'/'}><button className="button" role="button">Go home and start/break a new habit!</button></Link>
         </div>
     );
 }
