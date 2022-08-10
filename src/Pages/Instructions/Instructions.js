a01498ea98ce7548eaf60874fcbfe435b7b379b0
eddy-guo <eddyyanruguo@gmail.com>
2022-08-09 22:40:48 -0400

    general spacing, mobile spacing:

--- src/Pages/Instructions/Instructions.js
+++ src/Pages/Instructions/Instructions.js
@@ -3,19 +3,16 @@ import { Header } from '../../Exports';
 import { Link, useParams } from 'react-router-dom';
 import Authenticate from '../../Components/Altura/AuthenticationMessage';
 import BeginHabit from '../../Components/Altura/assignParent';
-import { useAccount } from 'wagmi';
 
 export default function Instructions() {
     const { type } = useParams();
-    const { address } = useAccount();
     return (
         <div>
             <Header title={'HabiPets'} />
             <h1 className="Title">Select a {type} Habit</h1>
             <p className="Body">Placeholder Instructions</p>
-            <span>{address ? address : 'no wallet connected'}</span>
-            <Authenticate/>
-            <BeginHabit style={{display:'flex'}}/>
+            <div className="authenticate"><Authenticate /></div>
+            <div className="begin"><BeginHabit /></div>
             <div className="NextPageBox">
                 <Link to={`/Goal${type}`}>Enter your Habit Details-&gt;</Link>
             </div>
