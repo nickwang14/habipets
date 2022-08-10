235639637ebf9be28c0b877715af4563fe88fc5c
JadenDurnford <jadendurnford@gmail.com>
2022-08-10 06:50:15 -0400

    adding transaction functionality

--- src/components/Log/Log.js
+++ src/components/Log/Log.js
@@ -1,9 +1,8 @@
 import PropTypes from 'prop-types'
 
-function Log({ streak, onClick, lastLog }) {
+function Log({ streak, lastLog }) {
     return (
         <div>
-            <button onClick={onClick}> Log Your Habit </button>
             <p> Your current streak is {streak} </p>
             <p> You logged your last Habit on {lastLog} </p>
         </div>
@@ -14,6 +13,5 @@ export default Log
 
 Log.propTypes = {
     streak: PropTypes.number,
-    onClick: PropTypes.func,
     lastLog: PropTypes.string,
 }
