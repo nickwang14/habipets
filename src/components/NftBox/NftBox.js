eca78c48fa2bb301ad3fa6c31fac7aa582d71d84
nickwang14 <nickwang14@gmail.com>
2022-08-10 04:44:42 -0400

    Add stateupdate back into nftbox

--- src/components/NftBox/NftBox.js
+++ src/components/NftBox/NftBox.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react'
 import PropTypes from 'prop-types';
 import './NftBox.css';
 import { AppContext } from 'ContextProvider';
@@ -7,33 +8,32 @@ import GoodPet from 'assets/GoodNft.png'
 import BadPet from 'assets/BadNft.png'
 
 export default function NftBox({ type }) {
+    const { pickHabitType } = useContext(AppContext)
     return (
-        <AppContext.Consumer>
-            {({ setHabitType }) => (
-                <div className="NftBoxContainer">
-                    <Link
-                        to={'/Instructions'}
-                        onClick={() => setHabitType(type)}
-                        className="NftBoxLink"
-                    >
-                        <div className='NftBox'>
-                            <img
-                                src={type === HabitType.GOOD ? GoodPet : BadPet}
-                                alt={`${type} Pet`}
-                                className="NftImage"
-                            />
-                            <div className="NftBoxTextDiv">
-                                <span className="NftBoxText">
-                                    {type === HabitType.GOOD
-                                        ? 'Embrace a good habit'
-                                        : 'Kick a bad habit'}
-                                </span>
-                            </div>
+        <AppContext>
+            <div className="NftBoxContainer">
+                <Link
+                    to={'/Instructions'}
+                    onClick={pickHabitType(type)}
+                    className="NftBoxLink"
+                >
+                    <div className='NftBox'>
+                        <img
+                            src={type === HabitType.GOOD ? GoodPet : BadPet}
+                            alt={`${type} Pet`}
+                            className="NftImage"
+                        />
+                        <div className="NftBoxTextDiv">
+                            <span className="NftBoxText">
+                                {type === HabitType.GOOD
+                                    ? 'Embrace a good habit'
+                                    : 'Kick a bad habit'}
+                            </span>
                         </div>
-                    </Link>
-                </div>
-            )}
-        </AppContext.Consumer>
+                    </div>
+                </Link>
+            </div>
+        </AppContext>
     );
 }
 
