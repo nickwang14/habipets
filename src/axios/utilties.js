acf0413bd4bcbf47c73425850dfadb563a60a5e4
JadenDurnford <jadendurnford@gmail.com>
2022-08-10 03:49:22 -0400

    deleting old utilities

--- src/axios/utilties.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const get = async (url, params) => {
-    return await axios.get(url, params)
-}
-
-export const post = async(url, params) => {
-    return await axios.post(url,params)
-}
