6810274144106a5152c1911320d6bc7042046848
JadenDurnford <jadendurnford@gmail.com>
2022-08-09 20:04:41 -0400

    adding authenticate to intro page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,12 @@
 import { Header, NftBox, Intro } from '../../Exports';
 import './Home.css';
 import { HabitType } from '../../consts';
-import Authenticate from '../../Components/Altura/AuthenticationMessage';
-import BeginHabit from '../../Components/Altura/assignParent';
 
 const Home = () => {
     return (
         <div>
             <Header title={'HabiPets'} />
-            <Authenticate/>
-            <BeginHabit style={{display:'flex'}}/>
+            <h1>Welcome</h1>
             <Intro />
             <div className="NftFlexBox">
                 <NftBox type={HabitType.GOOD} />
--- src/Pages/Instructions/Instructions.js
+++ src/Pages/Instructions/Instructions.js
@@ -1,15 +1,18 @@
 import './Instructions.css';
 import { Header } from '../../Exports';
 import { Link, useParams } from 'react-router-dom';
+import Authenticate from '../../Components/Altura/AuthenticationMessage';
+import BeginHabit from '../../Components/Altura/assignParent';
 
 export default function Instructions() {
     const { type } = useParams();
-
     return (
         <div>
             <Header title={'HabiPets'} />
             <h1 className="Title">Select a {type} Habit</h1>
             <p className="Body">Placeholder Instructions</p>
+            <Authenticate/>
+            <BeginHabit style={{display:'flex'}}/>
             <div className="NextPageBox">
                 <Link to={`/Goal${type}`}>Enter your Habit Details-&gt;</Link>
             </div>
