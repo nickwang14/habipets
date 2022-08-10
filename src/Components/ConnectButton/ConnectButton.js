bbd016629cbe810efb9c001cb042234233f89aee
nickwang14 <nickwang14@gmail.com>
2022-08-09 21:58:26 -0400

    Fix sourcemap errors, addconnectModal

---
+++ src/Components/ConnectButton/ConnectButton.js
@@ -0,0 +1,12 @@
+const ConnectButton = () => (
+    <button
+        className="ConnectButton"
+        onClick={() => {
+            alert('Altura Connect wallet backend');
+        }}
+    >
+        Connect to play
+    </button>
+)
+
+export default ConnectButton
