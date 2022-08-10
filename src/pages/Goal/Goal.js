5efbe3d46ccad641b8c017c86c3fcad10117d333
eddy-guo <eddyyanruguo@gmail.com>
2022-08-10 03:37:52 -0400

    habit button instead of link

--- src/pages/Goal/Goal.js
+++ src/pages/Goal/Goal.js
@@ -47,7 +47,7 @@ export default function Goal() {
                     </div>
                 </div>
                 <div className='InnerBox'>
-                    <button type='submit' form='HabitForm' value='submit'>Create My Habit</button>
+                    <button className="buttonText" type='submit' form='HabitForm' value='submit'>Create My Habit</button>
                 </div>
             </form>
         </div>
